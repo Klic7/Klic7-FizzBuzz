428a5fbbe2b7d1cec653ee8397a91dfc4ad23cc8
Klic7 <kylezaenger@gmail.com>
2023-03-02 01:21:27 +0000

    completed PopFizzBuzz

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,9 @@ public class Lab {
      * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
      */
     public String FizzBuzzGen(int n){
-        return "";
+        String pop = "";
+        if(n%3==0) pop = pop+"Fizz";
+        if(n%5==0) pop = pop+"Buzz";
+        return pop;
     }
 }
